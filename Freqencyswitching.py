900dcd3e996c598e04a0e5281201d9c1521fd45d
Elijah Zappe <53388831+emzappe@users.noreply.github.com>
2024-11-18 09:33:25 -0700

    Update Freqencyswitching.py

--- Freqencyswitching.py
+++ Freqencyswitching.py
@@ -1,20 +1,30 @@
 import time
+import sys
 import urllib.request
+freq1 = input("Enter Freqencycy one ")
+freq2 = input("Enter Freqency two ")
+freq3 = input("Enter Freqency three ")
+freq4 = input("Enter Freqency four ")
+base_ur = "http://10.42.0.10:5111/high_lo?freq="
+u1l = f"{base_ur}{freq1}"
+u2l = f"{base_ur}{freq2}"
+u3l = f"{base_ur}{freq3}"
+u4l = f"{base_ur}{freq4}"
 url = "http://10.42.0.10:5111/high_lo"
 for i in range(10000):
-  with urllib.request.urlopen('http://10.42.0.10:5111/high_lo?freq=7e9') as response:
+  with urllib.request.urlopen(u1l) as response:
    ht4l = response.read().decode('utf-8')
    print(ht4l)
    time.sleep(1)
-  with urllib.request.urlopen('http://10.42.0.10:5111/high_lo?freq=10e9') as response:
+  with urllib.request.urlopen(u2l) as response:
    ht3l = response.read().decode('utf-8')
    print(ht3l)
    time.sleep(1)
-  with urllib.request.urlopen('http://10.42.0.10:5111/high_lo?freq=15e9') as response:
+  with urllib.request.urlopen(u3l) as response:
    ht2l = response.read().decode('utf-8')
    print(ht2l)
    time.sleep(1)
-  with urllib.request.urlopen('http://10.42.0.10:5111/high_lo?freq=20e9') as response:
+  with urllib.request.urlopen(u4l) as response:
    html = response.read().decode('utf-8')
    print(html)
    time.sleep(1)
